ddad1d9180536934d82d42feb14655e37be3f76f
Nathanael Moody <hello@nathanaelphilip.com>
2020-05-22 08:32:29 -0500

    protects site

--- wp-content/themes/vigor/functions.php
+++ wp-content/themes/vigor/functions.php
@@ -1,11 +1,15 @@
 <?php
-    require_once 'vendor/autoload.php';
+  if ($GLOBALS['pagenow'] !== 'wp-login.php' && !is_user_logged_in()) {
+    wp_redirect( wp_login_url() ); exit;
+  }
 
-    require_once 'functions/assets.php';
-    require_once 'functions/setup.php';
+  require_once 'vendor/autoload.php';
 
-    require_once 'functions/acf.php';
-    require_once 'functions/shortcodes.php';
+  require_once 'functions/assets.php';
+  require_once 'functions/setup.php';
 
-    require_once 'functions/subscribe.php';
-    require_once 'functions/cart.php';
+  require_once 'functions/acf.php';
+  require_once 'functions/shortcodes.php';
+
+  require_once 'functions/subscribe.php';
+  require_once 'functions/cart.php';
